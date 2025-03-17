a918b63192aebed9bd06a17f9165c134b8657b63
sametcn99 <sametcn99@gmail.com>
2025-03-17 21:50:22 +0300

    .

--- test-project/.vitepress/theme/index.ts
+++ test-project/.vitepress/theme/index.ts
@@ -1,8 +1,6 @@
-// https://vitepress.dev/guide/custom-theme
 import { h, nextTick } from "vue";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
-import "./style.css";
 import { MermaidRenderer } from "vitepress-mermaid-renderer";
 import "vitepress-mermaid-renderer/dist/style.css";
 
@@ -11,12 +9,23 @@ export default {
   Layout: () => {
     return h(DefaultTheme.Layout, null, {});
   },
-  enhanceApp({ app, router, siteData }) {
+  enhanceApp({ app, router }) {
     const mermaidRenderer = MermaidRenderer.getInstance();
-    mermaidRenderer.initialize();
+    
+    // Initialize after app is mounted
+    app.mixin({
+      mounted() {
+        if (this.$root === this) {
+          mermaidRenderer.initialize();
+        }
+      }
+    });
 
+    // Handle route changes
     router.onAfterRouteChange = () => {
-      nextTick(() => mermaidRenderer.renderMermaidDiagrams());
+      nextTick(() => {
+        mermaidRenderer.renderMermaidDiagrams();
+      });
     };
   },
 } satisfies Theme;
