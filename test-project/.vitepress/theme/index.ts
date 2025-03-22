038198c0d600aca36b8be6fba69edc7cc49cbaa2
sametcn99 <sametcn99@gmail.com>
2025-03-22 23:07:46 +0300

    .

--- test-project/.vitepress/theme/index.ts
+++ test-project/.vitepress/theme/index.ts
@@ -1,5 +1,5 @@
 // https://vitepress.dev/guide/custom-theme
-import { h, nextTick } from "vue";
+import { h, nextTick, onMounted } from "vue";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import "./style.css";
@@ -9,7 +9,25 @@ import "vitepress-mermaid-renderer/dist/style.css";
 export default {
   extends: DefaultTheme,
   Layout: () => {
-    return h(DefaultTheme.Layout, null, {});
+    return h(DefaultTheme.Layout, null, {
+      // Add a mounted hook to ensure diagrams render on initial page load
+      'layout-top': () => h({
+        setup() {
+          onMounted(() => {
+            // This will run when the component is mounted on initial page load
+            nextTick(() => {
+              const mermaidRenderer = createMermaidRenderer({
+                theme: "default",
+                securityLevel: "loose",
+                startOnLoad: false,
+              });
+              mermaidRenderer.renderMermaidDiagrams();
+            });
+          });
+          return () => null;
+        }
+      })
+    });
   },
   enhanceApp({ app, router }) {
     // Use client-only safe implementation
